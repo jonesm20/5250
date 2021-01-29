30bab65ce62e2b111128c22340b59a5422fff891
jonesm20 <jonesm20@seattleu.edu>
2021-01-28 20:52:17 -0500

    Renamed ItemsPage to ItemIndexPage, changed ItemIndexPage.xaml, ItemIndexPage.xaml.cs, MainPage.xaml.cs

--- Mine/Mine/Views/MainPage.xaml.cs
+++ Mine/Mine/Views/MainPage.xaml.cs
@@ -28,7 +28,7 @@ namespace Mine.Views
                 switch (id)
                 {
                     case (int)MenuItemType.Items:
-                        MenuPages.Add(id, new NavigationPage(new ItemsPage()));
+                        MenuPages.Add(id, new NavigationPage(new ItemIndexPage()));
                         break;
                     case (int)MenuItemType.About:
                         MenuPages.Add(id, new NavigationPage(new AboutPage()));
