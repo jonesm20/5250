2760ecf7bafe1217ae59f4b9493f97dc853dea32
jonesm20 <jonesm20@seattleu.edu>
2021-02-01 01:28:42 -0500

    * ItemUpdatePage.xaml: * ItemUpdatePage.xaml.cs: Added ItemUpdatePage to project, changed   ItemUpdatePage.xaml, ItemUpdatePage.xaml.cs, Mine.csproj
    * ItemCreatePage.xaml: Updated Title. Changed ItemCreatePage.xaml
    
    * ItemDeletePage.xaml: Updated title. Changed ItemDeletePage.xaml
    
    * ItemIndexPage.xaml: Updated title. Changed ItemIndexPage.xaml
    
    * ItemReadPage.xaml: Updated title. Change to ItemReadPage.xaml

---
+++ Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Mine.Models;
+
+namespace Mine.Views.Items
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemUpdatePage : ContentPage
+    {
+        public ItemModel Item { get; set; }
+
+        public ItemUpdatePage()
+        {
+            InitializeComponent();
+
+            Item = new ItemModel
+            {
+                Text = "Item name",
+                Description = "This is an item description."
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "AddItem", Item);
+            await Navigation.PopModalAsync();
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Update the Display Value when the Stepper Changes
+        /// </summary>
+        void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        void Stepper_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+        }
+    }
+}
