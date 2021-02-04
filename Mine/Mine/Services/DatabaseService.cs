e4b7790c8bfe4e0a1a28c2f44fe4d3c3f3d48acc
jonesm20 <jonesm20@seattleu.edu>
2021-02-04 02:58:23 -0500

    Added CreateAsync to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -36,9 +36,19 @@ namespace Mine.Services
             }
         }
 
-        public Task<bool> CreateAsync(ItemModel item)
+        public async Task<bool> CreateAsync(ItemModel item)
         {
-            throw new NotImplementedException();
+            if (item == null)
+            {
+                return false;
+            }
+
+            var result = await Database.InsertAsync(item);
+            if (result == 0)
+            {
+                return false;
+            }
+            return true;
         }
 
         public Task<ItemModel> ReadAsync(string id)
