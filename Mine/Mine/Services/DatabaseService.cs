0f5aa9594c83a403505a308b161754f6f67bb8df
jonesm20 <jonesm20@seattleu.edu>
2021-02-03 23:58:07 -0500

    Added DatabaseService to services in shared project, DatabaseService.cs

---
+++ Mine/Mine/Services/DatabaseService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using SQLite;
+
+using Mine.Models;
+
+namespace Mine.Services
+{
+    public class DatabaseService
+    {
+        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
+        {
+            return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        });
+
+        static SQLiteAsyncConnection Database => lazyInitializer.Value;
+        static bool initialized = false;
+
+        public DatabaseService()
+        {
+            InitializeAsync().SafeFireAndForget(false);
+        }
+
+        async Task InitializeAsync()
+        {
+            if (!initialized)
+            {
+                if (!Database.TableMappings.Any(m => m.MappedType.Name == typeof(ItemModel).Name))
+                {
+                    await Database.CreateTablesAsync(CreateFlags.None, typeof(ItemModel)).ConfigureAwait(false);
+                }
+                initialized = true;
+            }
+        }
+    }
+}
