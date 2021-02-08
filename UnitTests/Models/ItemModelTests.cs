2856401103254a9881f52b59a14e9f2e5150a191
jonesm20 <jonesm20@seattleu.edu>
2021-02-08 12:45:34 -0500

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_Def_ault_Should_Pass()
+        {
+            //Arrange
+
+            //Act
+
+            //Reset
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
