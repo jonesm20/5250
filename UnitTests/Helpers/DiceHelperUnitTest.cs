2488e449ab0ec7885d0f3563c8e7aab2ad2a11ef
jonesm20 <jonesm20@seattleu.edu>
2021-02-08 16:15:47 -0500

    Added test for RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_and_6 to DiceHelperUnitText.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -38,6 +38,21 @@ namespace UnitTests.Helpers
         }
 
         [Test]
+        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_2_And_12()
+        {
+            //Arrange
+
+            //Act
+            var result = DiceHelpers.RollDice(2, 12);
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, result >= 2);
+            Assert.AreEqual(true, result <= 12);
+        }
+
+        [Test]
         public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
         {
             //Arrange
