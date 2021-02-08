1ab411d0ffffe12c66dc662f2ce55dc7b2172687
jonesm20 <jonesm20@seattleu.edu>
2021-02-08 15:02:41 -0500

    Added helper content to DiceHelper.cs in mine project

--- Mine/Mine/Helpers/DiceHelpers.cs
+++ Mine/Mine/Helpers/DiceHelpers.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+
 namespace Mine.Helpers
 {
-    public class DiceHelpers
+    public static class DiceHelpers
     {
-        public DiceHelpers()
+        private static Random rnd = new Random();
+        public static bool ForceRollsToNotRandom = false;
+        public static int ForcedRandomValue = 1;
+        public static int RollDice(int rolls, int dice)
         {
+            if (ForceRollsToNotRandom)
+            {
+                return rolls + ForcedRandomValue;
+            }
+            if (rolls < 1)
+            {
+                return 0;
+            }
+            if (dice < 1)
+            {
+                return 0;
+            }
+
+            var myReturn = 0;
+            for (var i = 0; i < rolls; i++)
+            {
+                myReturn += rnd.Next(1, dice + 1);
+            }
+            return myReturn;
         }
     }
 }
